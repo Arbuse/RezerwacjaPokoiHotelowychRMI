87a1e7e0c3d0f06e594486800ebdbc2358796639
Adam Dados <76404750+Arbuse@users.noreply.github.com>
2024-05-28 12:48:31 +0200

    Add files via upload

---
+++ RezerwacjaPokojowRMI/src/main/java/com/example/rezerwacjapokojowrmi/RezerwacjaPokojowRmiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rezerwacjapokojowrmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerwacjaPokojowRmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezerwacjaPokojowRmiApplication.class, args);
+	}
+
+}
---
+++ RezerwacjaPokojowRMI/src/test/java/com/example/rezerwacjapokojowrmi/RezerwacjaPokojowRmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rezerwacjapokojowrmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerwacjaPokojowRmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
